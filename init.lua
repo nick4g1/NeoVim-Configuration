250793676a3f292c7579cd0041df892e51cf3a55
nick4g1 <nicked2021@gmail.com>
2024-06-22 00:29:38 +0000

    more changes

--- init.lua
+++ init.lua
@@ -83,7 +83,8 @@ vim.opt.splitbelow = true
 -- Prime remaps
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
