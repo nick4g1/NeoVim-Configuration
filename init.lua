a7071f2d59246fb4ed85ba07a6f82f84f8cb116c
nick4g1 <nicked2021@gmail.com>
2024-06-22 00:06:39 +0000

    added some prime remaps

--- init.lua
+++ init.lua
@@ -40,6 +40,10 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+--vim.opt.colorcolumn = '80'
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = ''
@@ -75,6 +79,10 @@ vim.opt.timeoutlen = 300
 -- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+-- Prime remaps
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
